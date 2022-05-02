e2f1a8a0a53dc787c4f0185894f82009af577fb5
Osvaldo Gago <opgago@yahoo.com>
2022-05-02 12:54:51 +0100

    Formating js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = (function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.ignores.add("README.md");
-    return {
-        dir: {
-          output: "docs"
-        }
-      }
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.ignores.add("README.md");
+  return {
+    dir: {
+      output: "docs"
+    }
+  };
 });
