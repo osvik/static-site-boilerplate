e0794ca462d8fa5a0674450f79d2a317b9da542e
Osvaldo Gago <opgago@yahoo.com>
2022-05-02 09:22:15 +0100

    Don't make a page with the README

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = (function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.ignores.add("README.md");
     return {
         dir: {
           output: "docs"
