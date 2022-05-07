1a16512eef1079700ff22808bf9c208619761e26
Osvaldo Gago <opgago@yahoo.com>
2022-05-07 17:49:11 +0100

    Beautify

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
+/* jshint esversion:6 */
+
 module.exports = (function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.ignores.add("README.md");
-  return {
-    dir: {
-      output: "docs"
-    }
-  };
+
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.ignores.add("README.md");
+
+    return {
+        dir: {
+            output: "docs"
+        }
+    };
+    
 });
--- _data/site.js
+++ _data/site.js
@@ -1,19 +1,21 @@
 /* jshint esversion:6 */
-module.exports = function() {
+
+module.exports = function () {
+
     const environmentVal = process.env.MY_ENVIRONMENT || "development";
     let domainVal, indexval;
 
-    if ( environmentVal === "development") {
+    if (environmentVal === "development") {
         domainVal = "http://localhost:8080";
         indexval = false;
-    } else if ( environmentVal === "github" ) {
+    } else if (environmentVal === "github") {
         domainVal = "https://osvik.github.io/static-site-boilerplate";
         indexval = false;
     }
 
     return {
-      environment: environmentVal,
-      domain : domainVal,
-      index: indexval
+        environment: environmentVal,
+        domain: domainVal,
+        index: indexval
     };
-  };
+};
