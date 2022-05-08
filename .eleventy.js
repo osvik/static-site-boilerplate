631e071af647bb5a6e253f25b197e0455d9bb096
Osvaldo Gago <opgago@yahoo.com>
2022-05-08 17:24:27 +0100

    Year shortcode and author

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = (function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.ignores.add("README.md");
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
         dir: {
